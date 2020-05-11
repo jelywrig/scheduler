907109065f1ed12c2b2e8983407a99bc423a7842
Jessie Wright <jessie.wright@gmail.com>
2020-05-11 13:50:29 -0700

    initial version of button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 import "components/Button.scss";
+import classnames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+   let classNm = "button";
+   if(props.danger) {
+      classNm += " button--danger";
+   } else if (props.confirm) {
+      classNm += " button--confirm";
+   }
+
+   return <button disabled={props.disabled} onClick={ props.onClick } className={ classNm } >{ props.children }</button>;
 }
