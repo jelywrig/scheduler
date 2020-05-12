7e6722113969143610238de74f8b1262d822bd15
Jessie Wright <jessie.wright@gmail.com>
2020-05-12 13:02:17 -0700

    updated Interviewer list to controlled list naming convention

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,8 +14,8 @@ return (<section className="interviewers">
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer ? true: false}
-      setInterviewer={(event)=> props.setInterviewer(interviewer.id)}
+      selected={interviewer.id === props.value}
+      setInterviewer={(event)=> props.onChange(interviewer.id)}
     />
       })}
 </ul>
--- stories/index.js
+++ stories/index.js
@@ -117,13 +117,13 @@ storiesOf("Button", module)
         .add("Initial", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            onChange={action("onChange")}
           />
         ))
         .add("Preselected", () => (
           <InterviewerList
             interviewers={interviewers}
-            interviewer={3}
-            setInterviewer={action("setInterviewer")}
+            value={3}
+            onChange={action("onChange")}
           />
         ));
