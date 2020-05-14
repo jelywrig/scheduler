ae343fab2d33e6cf17adcc30a2e1b8b0ca165fe4
Jessie Wright <jessie.wright@gmail.com>
2020-05-14 11:57:58 -0700

    brought in interviewers data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,21 +3,24 @@ import axios from "axios";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import DayList from "components/DayList";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
-  const [state, setState] = useState({day: "Monday", days: [], appointments: {}});
+  const [state, setState] = useState({day: "Monday", days: [], appointments: {}, interviewers:{}});
   
   const setDay = day => setState({...state, day});
 
     useEffect(() => {
       Promise.all([
         Promise.resolve(axios.get("/api/days")),
-        Promise.resolve(axios.get("/api/appointments"))
+        Promise.resolve(axios.get("/api/appointments")),
+        Promise.resolve(axios.get("/api/interviewers"))
       ]).then((all)=> {
-        setState( prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+        setState( prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       })
     }, []);
+
+  
 
   return (
     <main className="layout">
@@ -43,7 +46,13 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {getAppointmentsForDay(state, state.day).map( appointment => {
-          return <Appointment key={appointment.id} {...appointment} />       
+          const interview = getInterview(state, appointment.interview);
+          return <Appointment 
+            key={appointment.id} 
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview}
+          />       
           
           })}
         <Appointment key="last" time="5pm" />
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 
 
-export default function getAppointmentsForDay(state, day){
+export function getAppointmentsForDay(state, day){
   const {days, appointments} = state;
   if(days.length === 0) {
     return [];
@@ -17,4 +17,15 @@ export default function getAppointmentsForDay(state, day){
     }
   }
   return result;
+};
+
+export function getInterview(state, interview) {
+  
+  if( interview && interview.interviewer){
+    const newInterview = { student: interview.student, interviewer: state.interviewers[interview.interviewer]};
+    return newInterview;
+  }
+
+  return null;
+
 };
