12d58267706af2cbab841dc0b78f605636b125bd
Jessie Wright <jessie.wright@gmail.com>
2020-05-14 16:41:56 -0700

    added edit option

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -17,6 +17,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
@@ -46,8 +47,9 @@ export default function Appointment(props) {
   return (<article className="appointment">
     <Header time={props.time} />
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-    {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} onDelete={()=> transition(CONFIRM)}/>}
+    {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} onDelete={()=> transition(CONFIRM)} onEdit={() => transition(EDIT)}/>}
     {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save} />}
+    {mode === EDIT && <Form interviewers={props.interviewers} name={props.interview.student} interviewer={props.interview.interviewer.id} onCancel={back} onSave={save}/>}
     {mode === SAVING && <Status message="Saving..."/>}
     {mode === DELETING && <Status message="Deleting..."/>}
     {mode === CONFIRM && <Confirm message="Are you shure you would like to delete?" onCancel={back} onConfirm={deleteAppointment}/>}
