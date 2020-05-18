765dd7fc52d0e67b72e27e18d976dbef11713482
Jessie Wright <jessie.wright@gmail.com>
2020-05-18 12:17:44 -0700

    added propType to InterviewList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
 
 
 
@@ -21,4 +22,10 @@ return (<section className="interviewers">
 </ul>
 </section>);
 
+}
+
+
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
 }
