a0e92f6b90d3ad598620e72c0b4b3b62d967597a
Jessie Wright <jessie.wright@gmail.com>
2020-05-19 16:40:07 -0700

    added another test to appplication

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, getByText, queryByText, prettyDOM, getAllByTestId, getByAltText, fireEvent, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, getByText, queryByText, prettyDOM, getAllByTestId, getByAltText, fireEvent, getByPlaceholderText, queryByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -46,4 +46,36 @@ describe("Application", () => {
     expect(getByText(monday, "no spots remaining")).toBeInTheDocument();
 
   })
+
+  it ("loads data, cancels an interview and increases the spots remaining for Monday by 1", async() => {
+    //1 render application
+    const { container } = render(<Application />);
+    
+    // 2 wait untiltext "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    //3 click the delete button
+    const appointment = getAllByTestId(container, "appointment").find( appointment => queryByText(appointment, "Archie Cohen"));
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+
+    // 4 check that the confirmation message is shown
+    expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
+
+    //5 click the confirm button
+    fireEvent.click(queryByText(appointment, "Confirm"));
+
+    //6 check that the element with the text "Deleting..."" is displayed
+
+    expect(getByText(appointment,"Deleting...")).toBeInTheDocument();
+
+    //7 wait until the element with the text add alt text is displayed
+
+    await waitForElement(() => getByAltText(appointment, "Add"));
+
+    //7 check that the daylistitem with the text Monday also has the text "2 spots remaining"
+
+    const monday = getAllByTestId(container, "day").find( day => queryByText(day, "Monday"));
+    expect(getByText(monday, "2 spots remaining")).toBeInTheDocument();
+    
+  })
+
 })
